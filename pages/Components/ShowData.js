72b0ef08920c51405acb008266670dda8cc7280a
suba <subalakshmimuthu02@gmail.com>
2023-03-20 16:57:42 +0530

    todo completed

--- pages/Components/ShowData.js
+++ pages/Components/ShowData.js
@@ -5,38 +5,38 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Button } from '@mui/material';
 import Router from 'next/router';
-import Link from 'next/link'
+import Link from 'next/link' 
+import Todo from '../Router/Todo';
+import Todos from './Todos';
 
-
-
-export default function showData({ data }) {
+export default function ShowData({ data }) {
   return (
     <div>
         {data.map((item, key) => {
-            return(
-                <Card>
-                  <CardContent>
-                    <Typography sx={{ fontSize: 14 }}>
-                      {item.id}
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }}>
-                      {item.name}
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }}>
-                      {item.username}
-                    </Typography>
-                    <Typography sx={{ fontSize: 14 }}>
-                      {item.email}
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                  <Link href={{ pathname: "/Router/Todo", id: {key}}}>
-                    <button type="button" size="small">Todo </button>
-                  </Link>
-                    {/* <Button size='small'>POST</Button>
-                    <Button size='small'>ALBUM</Button> */}
-                  </CardActions>
-                </Card>
+          return(
+            <Card>
+                <CardContent>
+                  {/* <Typography sx={{ fontSize: 14 }}>
+                    {item.id}
+                  </Typography> */}
+                  <Typography sx={{ fontSize: 14 }}>
+                    {item.name}
+                  </Typography>
+                  <Typography sx={{ fontSize: 14 }}>
+                    {item.username}
+                  </Typography>
+                  <Typography sx={{ fontSize: 14 }}>
+                    {item.email}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                <Link href={{ pathname: "/Router/Todo", query: {id: item.id}}}>
+                  <Button type="button" size="small">Todo</Button>
+                </Link>
+                  {/* <Button size='small'>POST</Button>
+                  <Button size='small'>ALBUM</Button> */}
+                </CardActions>
+              </Card>
             )
         })}
     </div>
--- pages/Components/Todos.js
+++ pages/Components/Todos.js
@@ -1,7 +1,50 @@
+import { Card } from '@mui/material';
 import React from 'react'
+import { useEffect, useState } from 'react'
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { useRouter } from 'next/router';
 
 export default function Todos() {
+
+    const router = useRouter();
+    const data = router.query;
+    console.log(data);
+
+    const [todoData, setTodoData] = useState([]);
+ 
+    async function fetchTodoData() {
+        const user = await fetch(`https://jsonplaceholder.typicode.com/todos?userId=${data.id}`);
+        const jsonData = await user.json();
+        setTodoData(jsonData);
+    }
+
+    useEffect(() => {
+        fetchTodoData();
+    },[])
+
   return (
-    <div></div>
+    <div>
+        {todoData.map((item,key) => {
+            return(
+                <Card>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }}>
+                            {item.userId}
+                        </Typography>
+                        <Typography sx={{ fontSize: 14 }}>
+                            {item.id}
+                        </Typography>
+                        <Typography sx={{ fontSize: 14 }}>
+                            {item.title}
+                        </Typography>
+                        <Typography sx={{ fontSize: 14 }}>
+                            {item.completed}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            )
+        })}
+    </div>
   )
 }
